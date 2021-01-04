c2f03e17f1aad9f896562f16710cc587d393dac9
kovackav <61971382+kovackav@users.noreply.github.com>
2021-01-04 14:19:23 +0100

    Add files via upload

---
+++ ukol2.py
@@ -0,0 +1,117 @@
+import json, os
+from pyproj import CRS, Transformer
+from math import sqrt
+import argparse
+
+path_kontejnery = "kontejnery.geojson"
+path_adresy = "adresy.geojson"
+
+'''Volitelny ukol, soubory jako parametry programu. -h zobrazi napovedu.'''
+parser = argparse.ArgumentParser(description = "Vypocet vzdalenosti ke kontejnerum na trideny odpad")
+parser.add_argument('-a', '--adresni_body', type = str, metavar = '', required = False, help = 'GeoJSON s adresnimi body.')
+parser.add_argument('-k', '--kontejnery', type = str, metavar = '', required = False, help = 'GeoJSON s kontejnery.')
+args = parser.parse_args()
+if args.adresni_body is not None:
+    path_adresy = args.adresni_body
+if args.kontejnery is not None:
+    path_kontejnery = args.kontejnery
+
+def load_file(path):
+    '''Nacteni vstupniho souboru. Pokud soubor chybi (spatne zadana cesta), vyskoci chyba.'''
+    try:
+        return open(path, encoding="UTF-8")
+    except FileNotFoundError:
+        print(f"Vstupni data {path} se nepodarilo nalezt, zkontrolujte cestu.")
+        exit()
+
+def load_json(file, path):
+    '''Cteni JSONu a kontrola, zdali je soubor validni.'''
+    try:
+        return json.load(file)["features"]
+    except ValueError: 
+        print(f"Soubor {path} neni validni GeoJSON.")
+        exit()
+
+def wgs2jtsk (latitude, longitude):
+    '''Funkce pro prevod z WGS do S-JTSK.'''
+    crs_wgs = CRS.from_epsg(4326) # WGS-84
+    crs_jtsk = CRS.from_epsg(5514) # S-JTSK
+    wgs2jtsk = Transformer.from_crs(crs_wgs,crs_jtsk)
+    jtsk = wgs2jtsk.transform(latitude,longitude)
+    return(jtsk)
+
+def pythagor(x, y):
+    '''Pythagorova veta.'''
+    return sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+
+def distance (adresy, kontejnery):
+    '''Vypocet vzdalenosti mezi adresnimi body a kontejnery. Vysledek uklada do slovniku.'''
+    distances = {}
+    for (adresy_street, adresy_coor) in adresy.items():
+        for (kontejnery_street, kontejnery_coor) in kontejnery.items():
+            dis = pythagor(adresy_coor, kontejnery_coor)
+            if dis > 10000:
+                print(f"Nejblizsi kontejner pro adresu {(adresy_street)} je dale, nez 10 km. Program skonci.")
+                exit()
+        distances[adresy_street] = dis
+    return distances
+
+def median(distances):
+    '''Vypocet medianu ze vzdalenosti.'''
+    v_list = []
+    for (street, dis) in distances.items(): #vytvoreni listu vzdalenosti
+        v_list.append(dis)
+    v_list.sort() #serazeni listu podle vzdalenosti
+    mid = (len(v_list)/2) #zjisteni prostredniho prvku
+    if not (mid % 2) == 0:  #pokud je median sude cislo 
+        mid1 = int(mid - 0.5)
+        mid2 = int(mid + 0.5)
+        med = (v_list[mid1]+v_list[mid2])/2
+    else:  #pokud je median liche cislo
+        med = v_list[mid]
+    return med
+
+loaded_file_kontejnery = load_file(path_kontejnery)
+loaded_file_adresy = load_file(path_adresy)
+
+json_kontejnery = load_json(loaded_file_kontejnery, path_kontejnery)
+json_adresy = load_json(loaded_file_adresy, path_adresy)
+
+#vytvoreni slovniku adres s pozadovanymi atributy
+adresy = {}
+for feature in json_adresy:
+    street = feature["properties"]["addr:street"] + " " + feature["properties"]["addr:housenumber"]
+    wgs_lat = feature["geometry"]["coordinates"][1]
+    wgs_lon = feature["geometry"]["coordinates"][0]
+    adresy[street] = wgs2jtsk(wgs_lat, wgs_lon) #volani funkce na prevod wgs do s-jtsk
+
+#vytvoreni slovniku kontejneru s pozadovanymi atributy
+kontejnery = {}
+for feature in json_kontejnery:
+    street = feature["properties"]["STATIONNAME"]
+    latlon = feature["geometry"]["coordinates"]
+    access = feature["properties"]["PRISTUP"]
+    # uvazujeme pouze volne pristupne kontejnery
+    if access=="volně":
+        kontejnery[street] = latlon
+
+print(f"Nacteno {len(adresy)} adresnich bodu.")
+print(f"Nacteno {len(kontejnery)} kontejneru na trideny odpad.")
+print()
+
+distances = distance(adresy, kontejnery) #vypocet vzdalenosti ke kontejnerum
+
+average = (sum(distances.values()) / len(distances)) #vypocet prumerne vzdalenosti ke kontejneru
+print(f"Prumerna vzdalenost ke kontejneru je {average:.0f} m.")
+
+maximum = max(distances.values()) #vypocet maximalni vzdalenosti ke kontejneru
+for (street, dis) in distances.items():
+    if dis == maximum:
+        maxstreet = street
+print(f"Nejdale ke kontejneru je z adresy {maxstreet} a to {maximum:.0f} m.")
+print()
+
+med = median(distances)
+print(f"Median vzdalenosti ke kontejneru je {med:.0f} m.")
+
+
